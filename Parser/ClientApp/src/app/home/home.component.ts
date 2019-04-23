5c0118476938a42ee716098d194fba5a39a98819
Максим Филипович <MAXFIL1998@outlook.com>
2019-04-23 18:22:19 +0300

    Parsed habr.com. Get articles, links and content news. Brought it all on html page.

--- Parser/ClientApp/src/app/home/home.component.ts
+++ Parser/ClientApp/src/app/home/home.component.ts
@@ -1,17 +1,23 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IWebDriverCookie, IWebElement, error } from 'selenium-webdriver';
+//import { ArticleLink } from './ArticleLink';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  public links: IWebElement[];
+  public ArticlesLinks: ArticleLink[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<IWebElement[]>(baseUrl + 'api/Default/TitleArticles').subscribe(result => {
-      this.links = result;
+    http.get<ArticleLink[]>(baseUrl + 'api/Default/GetTitleArticles').subscribe(result => {
+      this.ArticlesLinks = result;
     }, error => console.error(error));
   }
+}
+interface ArticleLink {
+  article: string;
+  link: string;
+  content: string;
 }
--- Parser/Controllers/DefaultController.cs
+++ Parser/Controllers/DefaultController.cs
@@ -7,7 +7,8 @@ using Parser.Models;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
-
+using AngleSharp;
+using AngleSharp.Dom;
 namespace Parser.Controllers
 {
     [Route("api/[controller]")]
@@ -18,12 +19,40 @@ namespace Parser.Controllers
             return View();
         }
         [HttpGet("[action]")]
-        public IWebElement TitleArticles()
+        public ArticleLink[] GetTitleArticles()
         {
-            IWebDriver driver=new ChromeDriver();
-            driver.Url = @"https://habr.com/ru/news/";
-            IWebElement links = (OpenQA.Selenium.IWebElement)driver.FindElements(By.ClassName("post__title_link"));
-            return links;
+            int i = 0;
+            ArticleLink[] articleLinks = new ArticleLink[100];
+            var config = Configuration.Default.WithDefaultLoader();
+            var URL = "https://habr.com/ru/news/";
+            var document = BrowsingContext.New(config).OpenAsync(URL).Result;
+            var Items = document.QuerySelectorAll("a.post__title_link");
+            foreach (var item in Items)
+            {
+                document = BrowsingContext.New(config).OpenAsync(item.GetAttribute("href").ToString()).Result;
+                articleLinks[i] = new ArticleLink { Article = item.Text(), Link = item.GetAttribute("href"),Content=GetContent(document) };
+                i++;
+            }
+            return articleLinks;
+
+        }
+        public string GetContent(IDocument document)
+        {
+            int count = 100;
+            var Content = document.QuerySelector("div.post__text").TextContent.ToString();
+                while(Content.Substring(count,1)!=" ")
+            {
+                count++;
+            }
+            string str = Content.Substring(0, count);
+            return str;
+        }
+
+        public class ArticleLink
+        {
+            public string Article { get; set; }
+            public string Link { get; set; }
+            public string Content { get; set; }
         }
     }
 }
